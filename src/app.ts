1355f3b97d248f2897c889d0587c8183f849b477
SviatoslavIvanchuk <isv1796@gmail.com>
2022-03-17 20:00:52 +0200

    Lesson5

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,64 @@
+import 'reflect-metadata';
 import express, { Request, Response } from 'express';
-
-import { users } from './users';
+import { createConnection, getManager } from 'typeorm';
+import { User } from './entity/user';
 
 const app = express();
-console.log(users);
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.get('/', (req: Request, res: Response) => {
-    res.end();
+app.get('/users', async (req: Request, res: Response) => {
+    const users = await getManager().getRepository(User).find({ relations: ['posts'] });
+    res.json(users);
+    // const users = await getManager().getRepository(User).findOne({
+    //     where: {
+    //         firstName: 'Olena',
+    //     },
+    // });
+    // const users = await getManager().getRepository(User).findOne();
+    // console.log(users);
+    // res.json(users);
+    // const users = await getManager().getRepository(User).find({ relations: ['posts'] });
+    // res.json(users);
+    // const users = await getManager().getRepository(User)
+    //     .createQueryBuilder('user')
+    //     .where('user.firstName = "Jaha"')
+    //     .getOne();
+    //
+    // console.log(users);
+    // res.json(users);
 });
 
-app.listen(5200, () => {
+app.post('/users', async (req, res) => {
+    const createdUser = await getManager().getRepository(User).save(req.body);
+    res.json(createdUser);
+});
+
+app.patch('/users/:id', async (req, res) => {
+    const { password, email } = req.body;
+    const createdUser = await getManager().getRepository(User)
+        .update({ id: Number(req.params.id) }, {
+            password,
+            email,
+        });
+    res.json(createdUser);
+});
+
+app.delete('/users/:id', async (req, res) => {
+    const createdUser = await getManager()
+        .getRepository(User)
+        .delete({ id: Number(req.params.id) });
+    res.json(createdUser);
+});
+
+app.listen(5200, async () => {
     console.log('Server has started!!!!');
+    try {
+        const connection = await createConnection();
+        if (connection) {
+            console.log('Database connected');
+        }
+    } catch (err) {
+        if (err) console.log(err);
+    }
 });
